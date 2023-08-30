e454f8145a06b4c459f3f8c71ca44c70d3b7e1c6
jcarloccs <jcarlo@outlook.com>
2023-08-30 12:35:09 -0300

    desafio concluído

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,38 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+
+        try {
+
+            System.out.println("Digite o primeiro parâmetro");
+            int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro");
+            int parametroDois = terminal.nextInt(); 
+		
+			contar(parametroUm, parametroDois);
+		
+		} catch (ParametrosInvalidosException exception) {
+            System.err.println(exception.getMessage());
+		} catch (InputMismatchException exception) {
+            System.err.println("Digite um número inteiro válido");
+        }
+
+        terminal.close();
+		
+	}
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) 
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+
+        int contagem = parametroDois - parametroUm;
+
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+
+}
